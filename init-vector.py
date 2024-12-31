c962f1607b1030829381d11c005175a28f2b1a65
cuipengfei <cuipengfei2008@gmail.com>
2024-12-31 20:19:58 +0800

    初始化向量数据库并添加文档处理功能
    - 新增 init-vector.py 文件，实现文档加载、分割和向量存储功能
    - 添加 introduction.txt 文件作为示例文档
    - 更新 .gitignore 文件，排除虚拟环境和向量数据库目录
    - 在 requirements.txt 中添加新的依赖项：
      - langchain-chroma
      - chromadb - dashscope
      - python-dotenv

---
+++ init-vector.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import DashScopeEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+loader = TextLoader("introduction.txt", encoding="utf-8")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma(
+    collection_name="ai_learning",
+    embedding_function=DashScopeEmbeddings(),
+    persist_directory="vectordb"
+)
+vectorstore.add_documents(splits)
+
+documents = vectorstore.similarity_search("豚母升木的研究内容有哪些？其引用的都资料有哪些？")
+print(documents)
